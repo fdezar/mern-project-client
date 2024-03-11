2e09c6f4996f9b87a2054f9c3b4a0633122a1647
Gabriel Fernandez <gfernandezareas@gmail.com>
2024-03-11 20:52:15 +0100

    vite-config changed

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const PROXY_URL = import.meta.env.VITE_PROXY_URL;
+
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5005',
+        target: PROXY_URL,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       }
