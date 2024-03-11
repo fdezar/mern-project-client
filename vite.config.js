71871d0b4a2dc1459365773d35a7925f4def22fd
Gabriel Fernandez <gfernandezareas@gmail.com>
2024-03-11 22:26:20 +0100

    loadenv

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'silk-project-server.vercel.app',
+        target: process.env.VITE_PROXY_URL,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       }
