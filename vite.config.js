f1e0d139cae6db6bd97e4f0dab58b2706c0ed956
Gabriel Fernandez <gfernandezareas@gmail.com>
2024-03-11 22:18:16 +0100

    commented config

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'silk-project-server.vercel.app',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      }
-    }
-  },
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'silk-project-server.vercel.app',
+  //       changeOrigin: true,
+  //       rewrite: (path) => path.replace(/^\/api/, ''),
+  //     }
+  //   }
+  // },
 
   // mirar documentación de Vite en server.proxy
   // path.replace. Target, changeorigin, rewrite
