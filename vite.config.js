9019921cf567638d83b87ec0fab4edfbbc18a68b
Gabriel Fernandez <gfernandezareas@gmail.com>
2024-03-11 21:33:25 +0100

    vercel

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: import.meta.env.VITE_PROXY_URL,
+        target: import.meta.env.VITE_BASE_URL,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       }
